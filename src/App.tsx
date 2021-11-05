53ec3853af83d44bd67683d9a305efd3c026c08b
Lucas Almeida <lucasaclima03@gmail.com>
2021-11-05 19:10:18 -0300

    adicionando typescript na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 export function App() {
-  return <RepositoryList />;
+  return (
+    <>
+      <RepositoryList />
+    </>
+  );
 }
